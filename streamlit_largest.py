07e10aad69cb422e1573eca901b362793af6f84e
Lakshmi <86044643+Sreelakshmi94@users.noreply.github.com>
2023-04-19 22:46:12 +0530

    Update streamlit_largest.py

--- streamlit_largest.py
+++ streamlit_largest.py
@@ -12,28 +12,30 @@ import pandas as pd
 
 #pip install streamlit
 
-st.title('Find the Largest Number!')
+def main():
+    
+    
+    st.title('Find the Largest Number!')
 
-st.header('Please enter 3 numbers to find the largest among them!!')
-
-def find_largest(num1,num2,num3):
-  if (num1 >= num2) and (num1 >= num3):
-    largest = num1
-  elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-  else:
-   largest = num3
-  return("The largest number among the three is ",largest)
-
-num1 = st.number_input("Enter first number")
-num2= st.number_input("Enter second number")
-num3= st.number_input("Enter third number")
-
-if st.button('Find Largest'):
-    large_num = find_largest(num1,num2,num3)
-    st.write(large_num)
+    st.header('Please enter 3 numbers to find the largest among them!!')
+    
+    num1 = st.number_input("Enter first number")
+    num2= st.number_input("Enter second number")
+    num3= st.number_input("Enter third number")
 
 
+    if st.button('Find Largest'):
+        
+        if (num1 >= num2) and (num1 >= num3):
+            largest = num1
+        elif (num2 >= num1) and (num2 >= num3):
+            largest = num2
+        else:
+            largest = num3
+        
+        st.write("The largest number among the three is : ",largest)
 
+
+streamlit.run streamlit_largest.py
 
 
